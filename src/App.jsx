68a857695ef346e1e99d970fbfb9296449819e57
Eduardo Avelar <ef.avelar@gmail.com>
2020-11-16 08:05:29 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import './App.css'
+import React from 'react'
+
+
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComIf'
+
+export default (props) => (
+    <div className="App">
+
+        <Card titulo="#06 - Condicional v2">
+            <CondicionalComIf numero={11}>
+            </CondicionalComIf>
+        </Card>
+
+        <Card titulo="#05 - Condicional v1">
+            <Condicional numero={11}>
+            </Condicional>
+        </Card>
+
+        <Card titulo="#04 - Repetição">
+            <Repeticao>
+            </Repeticao>
+        </Card>
+
+        <Card titulo="#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
+
+        <Card titulo="#02 - Componente com Parâmetro">
+            <ComParametro
+                titulo="Esse é o titulo"
+                subtitulo="Esse é o subtitulo">
+            </ComParametro>
+        </Card>
+
+
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+
+
+
+        {/**/}
+        {/*<Primeiro></Primeiro>
+
+<ComParametro
+    titulo="Opa"
+    subtitulo="Epa">
+</ComParametro> */}
+
+    </div>
+);
+
---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () =>
+    <>
+        <h1>Primeiro Component</h1>
+        <h2>Exemplo de um primeiro component em React</h2>
+    </>
